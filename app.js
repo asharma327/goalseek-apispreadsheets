a24f3ad52440db9950e7ba3187526c0d67297cf0
Adhaar Sharma <asharma327@gmail.com>
2025-03-29 01:43:30 +0700

    update port to be dynamic

--- app.js
+++ app.js
@@ -2848,8 +2848,9 @@ app.post("/api/calculate", (req, res) => {
 });
 
 // Start listening
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
